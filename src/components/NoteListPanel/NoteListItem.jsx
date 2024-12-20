351832ab9d7bb5b6ba20fcddba5bf62bb82adc02
azkacrows <azkacrows@gmail.com>
2024-12-20 18:39:47 +0700

    update

--- src/components/NoteListPanel/NoteListItem.jsx
+++ src/components/NoteListPanel/NoteListItem.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-export default function MyCard({ active, title, date, body }) {
+export default function MyCard({ active, title, date, body, onSelectedNoteId }) {
     const activeCard = active ? '!bg-primary/80' : '';
     const cardFullDate = new Date(date).toLocaleDateString('id-ID', {
         weekday: 'long',
