86f2e4a196202813634544445cd7e7168c37a137
Muhammad Jamal Luthfi <36999236+azkacrows@users.noreply.github.com>
2025-01-05 23:28:17 +0700

    Delete src/components/NoteEditorPanel directory

--- src/components/NoteEditorPanel/NoOpenedNotes.jsx
+++
@@ -1,7 +0,0 @@
-export default function NoteMainNotFound() {
-    return (
-        <div className="flex flex-col items-center justify-center h-full text-2xl text-white opacity-50">
-            No Opened Notes
-        </div>
-    );
-}
--- src/components/NoteEditorPanel/NoteToolbar/NoteWSIWYG/NoteWSIWYGEditor.jsx
+++
@@ -1,7 +0,0 @@
-export default function NoteWSIWYGEditor() {
-    return (
-        <div className="flex flex-row items-center justify-center fill-current">
-            <span>This will be WYSIWYG features soon</span>
-        </div>
-    );
-}
